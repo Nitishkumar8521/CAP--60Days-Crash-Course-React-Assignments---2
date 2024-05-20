69d223be9770b83aac85643ccf1fdf413fdc4fab
Nitish kumar singh <nkk7367095@gmail.com>
2024-05-20 23:57:53 +0530

    Making API call with Axios onClick event

---
+++ Make1/a2p1/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Posts from "./components/Posts";
+import Users from "./components/Users";
+import Post from "./components/Posts";
+import User from "./components/User";
+
+export default function App() {
+  return (
+    <>
+      <Users />
+      <Posts />
+    </>
+  );
+}
